4aa5f011cb16afe916b6624abbf24120689ae186
Wagner Sodré <wagnersodre@Wagners-MacBook-Pro.local>
2018-09-25 00:03:31 -0300

    created the post controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+    def index 
+        @content_first = 'This is some post example using ruby on rails 1';
+        @content_second = 'This is some post example using ruby on rails 2';
+    end
+    def new 
+        
+    end
+    def create 
+        
+    end
+    def edit 
+        
+    end
+    def update 
+        
+    end
+    def show 
+        
+    end
+    def destroy 
+        
+    end
+end
