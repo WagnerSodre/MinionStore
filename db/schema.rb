df22fbeeeb3c20640bfe12a55668bc0af57da441
Wagner Sodré <wagnersodre@Wagners-MacBook-Pro.local>
2018-09-26 00:17:10 -0300

    created the products model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_26_003414) do
+ActiveRecord::Schema.define(version: 2018_09_26_023137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 2018_09_26_003414) do
     t.text "body"
     t.integer "category_id"
     t.integer "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.float "price"
+    t.integer "stock"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
